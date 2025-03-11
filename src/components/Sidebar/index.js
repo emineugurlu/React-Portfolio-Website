a0ddecfcde23f61c78936af93a22e717f9be97d2
Emine Uğurlu <emineugurlu957@gmail.com>
2025-03-11 17:01:29 +0300

    Yeni eklenen özellikler

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '../../assets/images/logo-e.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -16,8 +17,42 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="active" to="/">
             <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
             </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
+            </NavLink>
         </nav>
 
+        <ul>
+            <li>
+                <a target="_blank" rel='norefrrer' href='https://www.linkedin.com/in/emine-ugurlu/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"></FontAwesomeIcon>
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='norefrrer' href='https://github.com/emineugurlu'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"></FontAwesomeIcon>
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='norefrrer' href='https://medium.com/@emineugurlu957'>
+                    <FontAwesomeIcon icon={faMedium} color="#4d4d4e"></FontAwesomeIcon>
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='norefrrer' href='https://www.instagram.com/emine__ugurlu__/'>
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e"></FontAwesomeIcon>
+                </a>
+            </li>
+            
+        </ul>
     </div>
 )
 
