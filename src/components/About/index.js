5c5a4142fbb811aa0defaa3fb51306978afc7254
Emine Uğurlu <emineugurlu957@gmail.com>
2025-04-02 22:27:16 +0300

    eklenen özellikler

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,22 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import { useState, useEffect } from 'react';
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+    }, []);
+
     return (
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters 
-                        letterClass="text-animate"
+                        letterClass={letterClass}
                         strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
                         idx={15} 
                     />
